a5bccc871d18fdc0380312678601ec61fbfc4431
ShlykovPavel <pavel14640@gmail.com>
2025-07-14 16:26:33 +0300

    add migrations

---
+++ internal/storage/database/migration/000002_create_booking_types_table.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE booking_types (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_booking_types_updated_at
+    BEFORE UPDATE ON booking_types
+    FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
